e41355357f838f8856bfb16703979d0181a1515b
Hannah, Joey <JHannah@globalspec.com>
2021-04-28 23:02:58 -0400

    feat: add initial get endpoints

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/convert/swop/{to}', function ($to, Request $request) {
+
+    $response = Http::withHeaders([
+        'Authorization' => 'ApiKey '.env('SWOP_KEY')
+    ])->get('https://swop.cx/rest/rates/EUR/'.$to);
+
+    return $response->json();
+});
+
+Route::get('/convert/rapidapi/{to}', function ($to, Request $request) {
+
+    $response = Http::withHeaders([
+        'x-rapidapi-key' => env('RAPID_KEY')
+    ])->get('https://currency-exchange.p.rapidapi.com/listquotes');
+
+    return $response->json();
+});
 
